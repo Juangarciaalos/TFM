150aba4f3129a7d19139eaaaf0e1aa55e8159777
Juangarciaalos <juangarciaalos2@gmail.com>
2026-04-29 18:54:57 +0200

    Modificado cargador wikitext, añadido PadToken

--- Codigo/modelos/GPT-2/load_wikitext2.py
+++ Codigo/modelos/GPT-2/load_wikitext2.py
@@ -2,10 +2,11 @@ from transformers import GPT2Tokenizer
 from datasets import load_dataset
 from torch.utils.data import DataLoader
 
-def load_wikitext2(batch_size=2):
+def load_wikitext2(batch_size=2, max_length=256):
     """
     Carga el dataset WikiText-2 para lenguaje (GPT-2).
     :param batch_size: tamaño del batch para DataLoader
+    :param max_length: longitud máxima de los tokens
     :return: train_loader, val_loader (DataLoaders para WikiText-2)
     """
     
@@ -14,14 +15,23 @@ def load_wikitext2(batch_size=2):
     #Cargar tokenizer de GPT-2
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 
-    #Tokenizar textos
+    tokenizer.pad_token = tokenizer.eos_token
+
     def encode(examples):
-        return tokenizer(examples['text'], return_tensors="pt", padding=True, truncation=True)
+        return tokenizer(examples['text'], 
+                         padding='max_length', 
+                         truncation=True, 
+                         max_length=max_length)
+
+
+    dataset = dataset.filter(lambda x: len(x['text'].strip()) > 10)
 
     train_dataset = dataset['train'].map(encode, batched=True)
     val_dataset = dataset['test'].map(encode, batched=True)
 
-    #Crear DataLoaders
+    train_dataset.set_format(type='torch', columns=['input_ids'])
+    val_dataset.set_format(type='torch', columns=['input_ids'])
+
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
